0fded7b6e3b0a9802d15303905d23213add9292d
Charles Jarvis <csjarvis43@gmail.com>
2022-06-06 17:14:51 -0600

    commit tree 4f0304a6d4fdbbc004095a501d30c93dd8d7a2f4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42)
+}
+
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(feet) {
+    let distanceTravelledInFeet = distanceFromHqInBlocks(feet);
+    return distanceTravelledInFeet * 264;
+
+}
+
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let dist = Math.abs(start - destination) * 264
+    if(dist < 400){
+        return 0
+    } else if(dist >= 400 && dist <=2000){
+        let price = (dist * .02 - 8).toFixed(2)
+        return parseFloat(price)
+    } else if(dist > 2000 && dist <= 2500){
+        return 25
+    } else return 'cannot travel that far'
+}
